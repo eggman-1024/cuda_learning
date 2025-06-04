35ceb4c8e238a94efc15a37066300d9fe8a766c0
ruitao zhang <3160391266@qq.com>
2025-06-04 14:08:00 +0800

    [refactor]: 添加了一下注释

--- 01_cuda_op/setup.py
+++ 01_cuda_op/setup.py
@@ -4,6 +4,10 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 setup(
     name="add2",
     include_dirs=["include"],
+    version='1.0',
+    author='rtzhang',
+    author_email='xxx@gmail.com',
+    description='cppcuda example',
     ext_modules=[
         CUDAExtension(
             "add2",
--- 01_cuda_op/src/add2_kernel.cu
+++ 01_cuda_op/src/add2_kernel.cu
@@ -1,3 +1,5 @@
+#include "../include/add2.h"
+
 __global__ void MatAdd(float* c, 
                       const float* a, 
                       const float* b, 
@@ -14,7 +16,7 @@ void launch_add2(float* c,
                  const float* a, 
                  const float* b, 
                  int n) {
-    dim3 block(16, 16);
-    dim3 grid(n/block.x, n/block.y);
-    MatAdd<<<grid, block>>>(c, a, b, n);
+    dim3 block(16, 16); // 定义每个block有16×16=256个线程
+    dim3 grid(n/block.x, n/block.y);    // 定义每个block有16×16=256个线程
+    MatAdd<<<grid, block>>>(c, a, b, n);    // 启动CUDA kernel
 }
